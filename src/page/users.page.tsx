0d995832a8521e37f21ce45fb6efd3d09a6e6407
Kolya Madridano <kolya.madridano@gmail.com>
2023-05-05 11:45:01 +0800

    initialize react-app

---
+++ src/page/users.page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Users() {
+  return <>
+    <p>
+    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Recusandae nulla, saepe vero, quidem eos beatae laboriosam officia excepturi optio corrupti nemo, ex aperiam incidunt molestiae libero possimus omnis itaque pariatur veniam sunt odit? Fugit, cupiditate officiis! Nam eum consequuntur, ad perferendis maxime consectetur! Laboriosam non eum voluptates temporibus fuga rerum ducimus voluptas quia eligendi adipisci doloremque, tenetur fugiat veniam. Dignissimos quam, odio eaque inventore est fuga saepe excepturi, itaque alias provident quae officiis fugit maxime harum? Consequatur velit quo molestias, error aut, delectus ab ipsa mollitia itaque incidunt sit iste voluptatibus distinctio pariatur reprehenderit saepe. Non, accusantium, temporibus, dolorem nobis labore libero delectus eius asperiores illum tempore inventore sequi dolore quod quasi minus ratione provident esse doloribus quo nam. Repellat odit perferendis similique maiores repellendus dignissimos, illum ipsum excepturi optio a at expedita voluptates ea, nostrum, velit ullam ab quam inventore beatae. Porro dolorem exercitationem, voluptates minus qui dicta quae dolores. Itaque quis praesentium minima facilis et, iste nisi ratione aut cum minus sint corrupti facere, enim sapiente officia, reprehenderit ad pariatur quam delectus repellat eos vero modi neque voluptatibus. Minima, iure, quia facere saepe maiores sed numquam nam ab omnis deserunt officia explicabo, illo nemo corporis! Corporis, molestiae corrupti.
+    </p>
+  </>
+}
+
+export default Users
